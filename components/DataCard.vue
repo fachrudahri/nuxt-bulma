de6415c4c1f229739851181b78a912ebf21fc882
fachrudahri <fachru.dahri@gmail.com>
2020-09-08 20:56:56 +0700

    initial commit

---
+++ components/DataCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="column is-4">
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image">
+                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">{{ name }}</p>
+              <p class="subtitle is-6 mt-5">@{{ username }}</p>
+              <p class="subtitle is-6">{{ email }}</p>
+              <p class="subtitle is-6">{{ phone }}</p>
+            </div>
+          </div>
+
+          <div class="content">
+            <a :href="website">{{ website }}</a>
+            <a href="#">#responsive</a>
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DataCard',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    website: {
+      type: String,
+      required: true
+    }
+  }
+
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="container">
+  <Navbar/>
+  <div class="columns is-multiline mt-5">
+    <DataCard v-for="(item, i) in items" :key="i"
+    :name="item.name"
+    :username="item.username"
+    :email="item.email"
+    :phone="item.phone"
+    :website="item.website"
+    />
+  </div>
+</div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar'
+import DataCard from '~/components/DataCard'
+import axios from 'axios'
+export default {
+  components: {
+    Navbar,
+    DataCard
+  },
+  data() {
+    return {
+      items: []
+    }
+  },
+  async mounted() {
+    const app = this
+    await axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      app.items = res.data
+    }).catch(err => console.log(err))
+  }
+}
+</script>
+
+<style>
+
+</style>
