d95e6d8f806634f3a00c5924c6bb194c98f25857
fachrudahri <fachru.dahri@gmail.com>
2020-09-09 04:27:59 +0700

    added new router

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -14,35 +14,8 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
-
-      <a class="navbar-item">
-        Documentation
-      </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
+      <router-link to="/" class="navbar-item">Home</router-link>
+      <router-link to="/Documentation" class="navbar-item">Documentation</router-link>
     </div>
 
     <div class="navbar-end">
---
+++ pages/Documentation/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Ini link Documentation <router-link to="/">Go back</router-link></h1>
+</template>
+
+<script>
+export default {
+  name: 'Documentation'
+}
+</script>
+
+<style>
+
+</style>
